9c865f533c0e1419804cf1158a9ec3485c2f7ae6
SamM922 <samuel.maturo@gmail.com>
2022-05-28 11:16:00 -0700

    Final

--- js/site.js
+++ js/site.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $(".img").hide();
-  let ansOne = 111;
-  let ansTwo = 222;
-  let ansThree = 333;
+  let ansOne = "176";
+  let ansTwo = "054";
+  let ansThree = "935";
   let solOne = false;
   let solTwo = false;
   let solThree = false;
@@ -10,21 +10,21 @@ $(document).ready(function(){
     let valOne = $("#number1").val();
     let valTwo = $("#number2").val();
     let valThree = $("#number3").val();
-    if (valOne.toString() === ansOne.toString()) {
+    if (valOne.toString() === ansOne) {
       $("#number1").css("background-color", "#03fc0b");
       solOne = true;
     } else {
       $("#number1").css("background-color", "red");
       solOne = false;
     }
-    if (valTwo.toString() === ansTwo.toString()) {
+    if (valTwo.toString() === ansTwo) {
       $("#number2").css("background-color", "#03fc0b");
       solTwo = true;
     } else {
       $("#number2").css("background-color", "red");
       solTwo = false;
     }
-    if (valThree.toString() === ansThree.toString()) {
+    if (valThree.toString() === ansThree) {
       $("#number3").css("background-color", "#03fc0b");
       solThree = true;
     } else {
